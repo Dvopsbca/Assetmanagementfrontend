0cbce3a70c795ba5c93a2290abda339fa4164dc4
Madhu Mitha <23suca41@tcarts.in>
2026-02-28 10:51:05 +0530

    Updated API URL

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import "./App.css";
 
-const API_URL = "http://localhost:8080/api/assets";
+const API_URL = "https://assetmanagementbackend-1.onrender.com/api/assets";
 
 function App() {
   const [assets, setAssets] = useState([]);
