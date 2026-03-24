ad22c9098993a00a2eccd51e4f913f9b228269e8
Madhu Mitha <23suca41@tcarts.in>
2026-03-24 09:49:50 +0530

    trigger redeploy after backend fix

--- src/App.js
+++ src/App.js
@@ -154,7 +154,7 @@ function App() {
 
   return (
     <div className="container">
-      <h2>Asset Management System - Frontend</h2>
+      <h2>Asset Management System </h2>
 
       <form onSubmit={handleSubmit}>
         <input name="assetName" placeholder="Asset Name" value={asset.assetName} onChange={handleChange} required />
